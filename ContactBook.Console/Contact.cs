5340a1ebe8bfd9c3ff2d7c1ce0ef7af089eb31c0
diakov2100 <diakov2100@gmail.com>
2016-10-10 10:03:01 +0300

    full upload

---
+++ ContactBook.Console/Contact.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+
+namespace Phonebook.Console
+{
+    public delegate void ContactChange(string newphone, Contact contact);
+    public class Contact
+    {
+        public event ContactChange TryChange;
+        private string firstName;
+        private string lastName;
+        private DateTime birthDate;
+        private string phone;
+        private string email;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                if ((value != null) && (value != "") && (value.Count(c => c == ' ') != value.Length))
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("First name cannot be null or empty string"));
+                }
+
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                if ((value != null) && (value != "") && (value.Count(c => c == ' ') != value.Length))
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException(String.Format("Last name cannot be null or empty string"));
+                }
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+
+            set
+            {
+                if ((value != null) && (value >= new DateTime(1900, 1, 1)))
+                {
+                    birthDate = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Birthdate cannot be earlier than 01 Jan 1900");
+                }
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+
+            set
+            {
+
+                TryChange?.Invoke(value, this);
+                if (value == null)
+                {
+                    phone = value;
+                }
+                else if ((value[0] == '+') && (value.Length > 4) && (value.Length <= 16) && (value.Length - 1 == value.Where(c => char.IsDigit(c)).Count()))
+                {
+                    phone = value;
+                }
+                else
+                {
+                    throw new ArgumentException("A phone number should begin with a “+”,followed by minimum 4 and maximum 15 digits");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    email = value;
+                }
+                else if ((value != "") && (value.Count(c => c == ' ') != value.Length))
+                {
+                    email = value.ToLower();
+                }
+                else
+                {
+                    throw new ArgumentException("E-mail cannot be an empty string");
+                }
+            }
+        }
+
+        public Contact(string FirstName, string LastName, DateTime BirthDate, string Phone, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.BirthDate = BirthDate;
+            this.Phone = Phone;
+            this.Email = Email;
+        }
+        public Contact(string FirstName, string LastName, DateTime BirthDate)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.BirthDate = BirthDate;
+            email = null;
+            phone = null;
+        }
+    }
+}
---
+++ ContactBook.Console/ContactBook.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Phonebook.Console
+{
+
+    public class ContactBook
+    {
+        private List<Contact> allContacts = new List<Contact>();
+        private Dictionary<string, Contact> allContactsDictionary = new Dictionary<string, Contact>();
+
+        public List<Contact> AllContacts
+        {
+            get
+            {
+                return allContacts;
+            }
+        }
+        public void AddContact(Contact contact)
+        {
+
+            if (((contact.Phone != null) && (allContactsDictionary.ContainsKey(contact.Phone))) || ((contact.Email != null) && (allContacts.Exists(c => contact.Email == c.Email))))
+            {
+                throw new ArgumentException(String.Format("Contact with such data has been already added"));
+            }
+            else
+            {
+                allContacts.Add(contact);
+                contact.TryChange += TryChange;
+                if (contact.Phone != null)
+                {
+                    allContactsDictionary[contact.Phone] = contact;
+                }
+            }
+
+        }
+
+        public List<Contact> SearchByName(string expression)
+        {
+            return allContacts.FindAll(c =>
+               ((Regex.IsMatch(expression, c.FirstName, RegexOptions.IgnoreCase)) && !(Regex.IsMatch(expression, c.LastName, RegexOptions.IgnoreCase)))
+            || ((Regex.IsMatch(expression, c.LastName, RegexOptions.IgnoreCase)) && !(Regex.IsMatch(expression, c.FirstName, RegexOptions.IgnoreCase)))
+            || ((Regex.IsMatch(expression, c.FirstName, RegexOptions.IgnoreCase)) && (Regex.IsMatch(expression, c.LastName, RegexOptions.IgnoreCase)))
+            );
+        }
+
+        public Contact SearchByEmail(string email)
+        {
+            return allContacts.Find(c => email == c.Email);
+        }
+
+        public Contact SearchByPhone(string phone)
+        {
+            if (allContactsDictionary.ContainsKey(phone))
+            {
+                return allContactsDictionary[phone];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void TryChange(string newphone, Contact contact)
+        {
+            if ((newphone != null) && (allContacts.Find(c => (newphone == c.Phone)&&(c != contact)) != null))
+            {
+                throw new Exception("Contact with such number has been already added");
+            }
+            
+            if ((contact.Phone == null) && (newphone != null))
+            {
+                allContactsDictionary[newphone] = contact;
+            }
+            else if (((contact.Phone != null) && (newphone == null)))
+            {
+                allContactsDictionary.Remove(contact.Phone);
+            }
+            else if (((contact.Phone != null) && (newphone != null)))
+            {
+                allContactsDictionary.Remove(contact.Phone);
+                allContactsDictionary[newphone] = contact;
+            }
+
+        }
+        public void RemoveAll(Func<Contact, bool> check)
+        {
+            foreach (Contact currentContact in allContacts)
+            {
+                if (check(currentContact))
+                {
+                    currentContact.TryChange -= TryChange;
+                    if (currentContact.Phone != null) { allContactsDictionary.Remove(currentContact.Phone); }
+                }
+            }
+            allContacts.RemoveAll(c => check(c));
+        }
+    }
+}
